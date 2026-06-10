2367a4d7863f6fff8acec8759ebc7e482d61a372
basarievmaksim-maker <basarievmaksim@gmail.com>
2026-06-10 15:43:59 +0500

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+const handler = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -19,4 +19,6 @@ export default async function handler(req, res) {
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
-}
+};
+
+module.exports = handler;
